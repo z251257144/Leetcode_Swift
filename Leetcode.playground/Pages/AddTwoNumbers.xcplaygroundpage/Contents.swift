334437969d21491a23591cecbdc1b7c45e2f28ae
z251257144 <zmsoul@126.com>
2019-06-17 09:51:04 +0800

    实现两数相加算法

--- Leetcode.playground/Pages/AddTwoNumbers.xcplaygroundpage/Contents.swift
+++ Leetcode.playground/Pages/AddTwoNumbers.xcplaygroundpage/Contents.swift
@@ -3,43 +3,66 @@ import UIKit
 var str = "Hello, playground"
 print(str)
 
-public class ListNode {
+public class ListNode : CustomStringConvertible {
     public var val: Int
     public var next: ListNode?
     public init(_ val: Int) {
         self.val = val
         self.next = nil
     }
+    
+    public var description: String {
+        var string = "\(self.val)"
+        
+        var next = self.next
+        while next != nil {
+            string += "->"
+            string += "\(next!.val)"
+            next = next?.next
+        }
+        
+        return string
+    }
 }
 
 class Solution {
     func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
-        guard let p = l1 else { return nil }
-        guard let q = l2 else { return nil }
         
-        var result = ListNode(0)
+        var p = l1, q = l2
+        let result = ListNode(0)
+        var currentNode : ListNode = result
         var carry = 0
+        var sum = 0
+        
         while p != nil || q != nil {
-            let i = p.next != nil ? p.val : 0
-            let j = p.next != nil ? p.val : 0
-            let sum : Int = i+j + carry
+            sum = (p?.val ?? 0) + (q?.val ?? 0) + carry
             carry = sum / 10
-            result.next = ListNode(sum%10)
-            result = result.next
+            currentNode.next = ListNode(sum%10)
+            currentNode = currentNode.next!
             
-            if p != nil {
-                p = p.next
-            }
-            
-            if q != nil {
-                q = q.next
-            }
+            p = p?.next
+            q = q?.next
         }
         
         if carry > 0 {
-            result.next = ListNode(carry)
+            currentNode.next = ListNode(carry)
         }
         
-        return result
+        return result.next
     }
 }
+
+let l1 = ListNode(3)
+l1.next = ListNode(4)
+//l1.next?.next = ListNode(7)
+print(l1)
+
+let l2 = ListNode(1)
+l2.next = ListNode(7)
+l2.next?.next = ListNode(9)
+print(l2)
+
+let l3 = Solution().addTwoNumbers(l1, l2)
+print(l3!)
+
+
