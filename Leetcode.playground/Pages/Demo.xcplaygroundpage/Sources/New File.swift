c0cfb9422e48f9e681f468dbdbf93c7dd0134a54
z251257144 <zmsoul@126.com>
2019-11-04 22:19:25 +0800

    复习TwoSum算法

---
+++ Leetcode.playground/Pages/Demo.xcplaygroundpage/Sources/New File.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+class Demo1: NSObject {
+
+    /// 暴力解法
+    static func violentSolution(_ nums: [Int], target: Int) -> [Int] {
+        let result = [Int]()
+        
+        return result
+    }
+    
+    /// 优化解法
+    static func betterSolution(_ nums: [Int], target: Int) -> [Int] {
+        let result = [Int]()
+        
+        return result
+    }
+    
+    /// 最优解法
+    static func bestSolution(_ nums: [Int], target: Int) -> [Int] {
+        let result = [Int]()
+        
+        return result
+    }
+}
--- Leetcode.playground/Pages/TwoSum.xcplaygroundpage/Contents.swift
+++ Leetcode.playground/Pages/TwoSum.xcplaygroundpage/Contents.swift
@@ -19,76 +19,79 @@
 
 import Foundation
 
-var str = "Hello, playground"
-
 let nums = [2, 7, 11, 15]
-
-//: 这暴力解法，性能最差，时间复杂度：n^2
-func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-    var result:[Int] = []
-    
-    for (i, iValue) in nums.enumerated() {
-        for (_, jValueJ) in nums.enumerated() {
-            if iValue+jValueJ == target {
-                result.append(i);
-            }
-        }
-    }
-    
-    return result
-}
-
-let result = twoSum(nums, 18)
-print("暴力解法结果：")
-print(result)
+print("暴力解法：\(TwoSum1.violentSolution(nums, target: 18))")
+print("优化解法：\(TwoSum1.betterSolution([3,3], target: 18))")
+print("最优解法：\(TwoSum1.bestSolution(nums, target: 18))")
 
 
-//: 优化解法，时间复杂度：n*2
-func twoSumBetter(_ nums: [Int], _ target: Int) -> [Int] {
-    var result:[Int] = []
-    
-    var dict = [Int: Int]()
-    for (index, value) in nums.enumerated() {
-        dict[value] = index
-    }
 
-    for (index, value) in nums.enumerated() {
-        let temp = target - value
-        if let i = dict[temp] {
-            result.append(index);
-            result.append(i);
-            break
-        }
-    }
-
-    return result
-}
-
-let resultBetter = twoSumBetter(nums, 18)
-print("优化解法结果：")
-print(resultBetter)
-
-
-//: 最优解法，时间复杂度：n
-func twoSumBest(_ nums: [Int], _ target: Int) -> [Int] {
-    var result:[Int] = []
-
-    var dict = [Int: Int]()
-    
-    for (index, value) in nums.enumerated() {
-        let temp = target - value
-        if let i = dict[temp] {
-            result.append(i);
-            result.append(index);
-            break
-        }
-        
-        dict[value] = index
-    }
-
-    return result
-}
-let resultBest = twoSumBest(nums, 18)
-print("最优解法结果：")
-print(resultBetter)
+////: 这暴力解法，性能最差，时间复杂度：n^2
+//func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+//    var result:[Int] = []
+//
+//    for (i, iValue) in nums.enumerated() {
+//        for (_, jValueJ) in nums.enumerated() {
+//            if iValue+jValueJ == target {
+//                result.append(i);
+//            }
+//        }
+//    }
+//
+//    return result
+//}
+//
+//let result = twoSum(nums, 18)
+//print("暴力解法结果：")
+//print(result)
+//
+//
+////: 优化解法，时间复杂度：n*2
+//func twoSumBetter(_ nums: [Int], _ target: Int) -> [Int] {
+//    var result:[Int] = []
+//
+//    var dict = [Int: Int]()
+//    for (index, value) in nums.enumerated() {
+//        dict[value] = index
+//    }
+//
+//    for (index, value) in nums.enumerated() {
+//        let temp = target - value
+//        if let i = dict[temp] {
+//            result.append(index);
+//            result.append(i);
+//            break
+//        }
+//    }
+//
+//    return result
+//}
+//
+//let resultBetter = twoSumBetter(nums, 18)
+//print("优化解法结果：")
+//print(resultBetter)
+//
+//
+////: 最优解法，时间复杂度：n
+//func twoSumBest(_ nums: [Int], _ target: Int) -> [Int] {
+//    var result:[Int] = []
+//
+//    var dict = [Int: Int]()
+//
+//    for (index, value) in nums.enumerated() {
+//        let temp = target - value
+//        if let i = dict[temp] {
+//            result.append(i);
+//            result.append(index);
+//            break
+//        }
+//
+//        dict[value] = index
+//    }
+//
+//    return result
+//}
+//let resultBest = twoSumBest(nums, 18)
+//print("最优解法结果：")
+//print(resultBetter)
 
